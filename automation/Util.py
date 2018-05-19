9a6da1cb88ec98e86ffa38fd874d47f928a39e8c
Alan Coon <alancoon@usc.edu>
2018-05-19 12:47:16 -0700

    Adjust bot_request.json and Util.py

--- automation/Util.py
+++ automation/Util.py
@@ -8,7 +8,7 @@ Retrieves the user's GroupMe access token from /data/groupme_access_token.json a
 returns it. Returns "-1" if credentials file cannot be found.
 '''
 def _get_access_token():
-	credentials_path = os.path.join('.', 'data', 'groupme_access_token.json')
+	credentials_path = os.path.join('..', 'data', 'groupme_access_token.json')
 	if not os.path.exists(credentials_path):
 		print("Error loading GroupMe access token from /data/groupme_access_token.json")
 		return "-1"
